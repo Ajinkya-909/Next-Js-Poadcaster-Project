e784732a51e9afc7310b1a21e9449f7fd83c772f
Ajinkya Deshmukh <ajinkyadeshmukh8686@gmail.com>
2025-04-08 18:40:31 +0530

    Initiating Project and all necessary Routes

---
+++ app/(root)/poadcast/[poadcastId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PoadcastDetails = ({params}:{params:{poadcastId:string}}) => {
+  return (
+    <div>PoadcastDetails</div>
+  )
+}
+
+export default PoadcastDetails
